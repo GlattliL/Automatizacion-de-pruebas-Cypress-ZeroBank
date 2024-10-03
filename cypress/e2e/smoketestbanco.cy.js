a95080d95bae0d49ec09cb9973fb822d28f95330
lautaroezeglattli@gmail.com <lautaroezeglattli@gmail.com>
2024-10-03 17:46:43 -0300

    Subir carpeta completa

--- cypress/e2e/smoketestbanco.cy.js
+++ cypress/e2e/smoketestbanco.cy.js
@@ -9,7 +9,7 @@ it("Validar página de inicio,", () => {
 
     cy.visit("http://zero.webappsecurity.com/")
     cy.get(".active > img").should("be.visible")
-    cy.get(".active > .custom > h4").contains("Online Banking")
+    cy.get(".active > .custom > h4").contains("Online Banking" )
 
 
 })
